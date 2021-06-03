c50cb4928587d90a804a589f29657a8f8ba1b7de
nta228 <64982003+nta228@users.noreply.github.com>
2021-06-03 15:48:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     introTitle: 'Gửi Iem Pé = Pông !',
     introDesc: 'They say as long as at least one person cares for you, life isn’t a waste. So when things go terribly wrong, and you feel like giving up, please remember you still got me.',
     btnIntro: 'From little Tanhtanh with love, hehe ',
-    title: 'Can I say I love you today? If not, can I ask you again tomorrow? And the day after tomorrow? And the day after that? Coz I’ll be loving you every sinhgle day of my life? 😙',
+    title: 'Can I say I love you today? If not, can I ask you again tomorrow? And the day after tomorrow? And the day after that? Coz I’ll be loving you every single day of my life? 😙',
     desc: 'Will you love me tomorrow? ',
     btnYes: 'OK a iu <33',
     btnNo: 'Không, Anh trai à :3',
